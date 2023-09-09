0c0f7895d91b8cebfea6ea864297dbb2d788eb5b
Hadi <hadimktri@gmail.com>
2023-09-09 01:10:12 -0700

    search input and date input added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "@/components/home/Hero";
+import SearchInput from "@/components/home/SearchInput";
 
 export default function Home() {
   return (
     <div className="p-5 sm:px-8 md:px-20">
       <Hero/>
+      <SearchInput/>
     </div>
   );
 }
---
+++ src/components/home/SearchInput.tsx
@@ -0,0 +1,28 @@
+import { TiLocation } from "react-icons/ti";
+export default function SearchInput() {
+  return (
+    <div className="mt-5">
+      <h2 className="text-center text-[20px] text-gray-400 my-3">
+        Lets Search what you need
+      </h2>
+      <div className="flex justify-center">
+        <div className="flex bg-gray-100 rounded-full p-1 pb-2 divide-x">
+          <div className="text-gray-400 flex gap-2 items-center">
+            <TiLocation size={25} color="black" />
+            <input
+              type="text"
+              placeholder="Location"
+              className="p-2 outline-none bg-transparent"
+            />
+          </div>
+          <div>
+            <input
+              type="date"
+              className="p-2 outline-none bg-transparent  text-gray-400"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
