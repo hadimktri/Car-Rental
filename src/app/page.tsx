a7f6bad2abab0499d2bacadad18bd4a7e078d89c
Hadi <hadimktri@gmail.com>
2023-09-09 01:35:44 -0700

    cars filter section added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
+import CarsFiltersOptions from "@/components/home/CarsFiltersOptions";
 import Hero from "@/components/home/Hero";
 import SearchInput from "@/components/home/SearchInput";
 
 export default function Home() {
   return (
     <div className="p-5 sm:px-8 md:px-20">
-      <Hero/>
-      <SearchInput/>
+      <Hero />
+      <SearchInput />
+      <CarsFiltersOptions />
     </div>
   );
 }
---
+++ src/components/home/CarsFiltersOptions.tsx
@@ -0,0 +1,27 @@
+export default function CarsFiltersOptions() {
+  return (
+    <div className="mt-10 flex items-center justify-between">
+      <div>
+        <h2 className="text-[30px] font-bold">Cars Catalog</h2>
+        <h2>Explore our cars you might like</h2>
+      </div>
+      <div className="flex gap-5">
+        <select className="select select-bordered w-full max-w-xs">
+          <option disabled selected>
+            Price
+          </option>
+          <option>Min to Max</option>
+          <option>Max to Min</option>
+        </select>
+        <select className="select select-bordered w-full md:block max-w-xs hidden">
+          <option disabled selected>
+            Manufacture
+          </option>
+          <option>Honda</option>
+          <option>Toyota</option>
+          <option>BMW</option>
+        </select>
+      </div>
+    </div>
+  );
+}
